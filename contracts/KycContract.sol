b83c1f0183873809002e617091cf79cdc1434068
Tanner Brandt <tannerbrandt@Tanners-iMac.local>
2022-02-17 18:02:34 -0800

    Contract Updates
    Updated MyTokenSale.sol to allow any address to make a contract call to whitelist their address. This would usually be an action only the owner could take, but I'm changing the code so that anyone can interact with the contract once deployed.

--- contracts/KycContract.sol
+++ contracts/KycContract.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-
+//import "@openzeppelin/contracts/access/Ownable.sol";
+/** For this test app, anyone can whitelist their address, not just the owner
 contract KycContract is Ownable {
     mapping(address => bool) allowed;
 
@@ -10,6 +10,23 @@ contract KycContract is Ownable {
     }
 
     function setKycRevoked(address _addr) public onlyOwner {
+        allowed[_addr] = false;
+    }
+
+    function kycCompleted(address _addr) public view returns(bool) {
+        return allowed[_addr];
+    }
+}
+*/
+
+contract KycContract {
+    mapping(address => bool) allowed;
+
+    function setKycCompleted(address _addr) public {
+        allowed[_addr] = true;
+    }
+
+    function setKycRevoked(address _addr) public {
         allowed[_addr] = false;
     }
 
